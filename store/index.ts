1c7b0977a611fbb1df3b0e98df9fa3d06fa087a4
LUCIEN BOULLEZ <lucien.boullez78@gmail.com>
2023-04-01 12:06:12 +0200

    Slice Error Store Reducer / Action

--- store/index.ts
+++ store/index.ts
@@ -8,6 +8,7 @@ import { createWrapper } from "next-redux-wrapper";
 import thunk from "redux-thunk";
 import { loaderSlice } from "./slices/loaderSlice";
 import { traitSlice } from "./slices/traitSlice";
+import { errorSlice } from "./slices/errorSlice";
 
 const enhancer = applyMiddleware(thunk);
 
@@ -16,6 +17,7 @@ const makeStore = () =>
     reducer: {
       [loaderSlice.name]: loaderSlice.reducer,
       [traitSlice.name]: traitSlice.reducer,
+      [errorSlice.name]: errorSlice.reducer,
     },
     devTools: true,
     enhancers: [enhancer],
