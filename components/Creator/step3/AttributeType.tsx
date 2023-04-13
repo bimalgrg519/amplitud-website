4e834e510002f800a1b9769ed6c181aa423b10c8
Wallace Ferreira <54377956+wallace-df@users.noreply.github.com>
2023-04-13 13:29:16 -0300

    Update AttributeType.tsx

--- components/Creator/step3/AttributeType.tsx
+++ components/Creator/step3/AttributeType.tsx
@@ -115,7 +115,7 @@ export default connect(
                      console.log(value);
 
                      updateAtributeWeight({
-                       attribute: { ...attribute, weight: ((value as number) / 100.0) },
+                       attribute: { ...attribute, weight: new Number(value) / 100.0 },
                        trait: trait.id,
                      });
                    }}
